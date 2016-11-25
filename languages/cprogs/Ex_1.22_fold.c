c01dde9ba6e70f196fbc3da6441ff54ad39d0693
GabrielSaul <gabrielsaul.commerce@outlook.com>
2016-11-25 01:45:06 +0000

    Update Ex_1.22_fold.c

--- languages/cprogs/Ex_1.22_fold.c
+++ languages/cprogs/Ex_1.22_fold.c
@@ -1,6 +1,6 @@
 /* Exercise 1-22: Write a program to "fold" long input lines into two or more
-				  shorter lines after the last non-blank character that occurs
-	 			  before the n-th column of input. */
+	          shorter lines after the last non-blank character that occurs
+	          before the n-th column of input. */
 				  
 #include <stdio.h>
 
